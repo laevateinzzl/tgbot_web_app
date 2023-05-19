3850842ea3678706b72e466fa159373095892257
zzl <zhaoaa966@163.com>
2023-05-19 17:04:23 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {} from "telegram-webapps-types";
+// import { ref } from "vue";
+
+const tele = Telegram.WebApp;
+
+console.log(tele);
+</script>
+
+<template></template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
