0b1a16c9059e10a58a3fb0f0186abf6ecd89d22c
zzl <zhaoaa966@163.com>
2023-05-20 10:13:43 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import {} from "telegram-webapps-types";
 import { onMounted } from "vue";
-// import { ref } from "vue";
+import { ref } from "vue";
 
 const tele = Telegram.WebApp;
+
+const count = ref(0);
 
 onMounted(() => {
   tele.ready();
@@ -11,7 +13,12 @@ onMounted(() => {
   console.log(tele);
 </script>
 
-<template>TestWebApp</template>
+<template>
+  <div>TestWebApp</div>
+  <div>
+    <button @click="count++">count is: {{ count }}</button>
+  </div>
+</template>
 
 <style scoped>
 .read-the-docs {
